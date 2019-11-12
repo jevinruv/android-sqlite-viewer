b784b6de6aa43ef21836732a8e6b56f31d221042
Jevin Fernando <jevin.fernando@ifsworld.com>
2019-11-12 09:39:34 +0530

    added viewer and configs

---
+++ SQLiteViewer/Viewer.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SQLiteViewer
+{
+    partial class Viewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.inputQuery = new System.Windows.Forms.RichTextBox();
+            this.btnExecute = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 114);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(857, 367);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.Location = new System.Drawing.Point(785, 43);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(85, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // inputQuery
+            // 
+            this.inputQuery.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputQuery.Location = new System.Drawing.Point(13, 12);
+            this.inputQuery.Name = "inputQuery";
+            this.inputQuery.Size = new System.Drawing.Size(754, 96);
+            this.inputQuery.TabIndex = 6;
+            this.inputQuery.Text = "";
+            this.inputQuery.TextChanged += new System.EventHandler(this.inputQuery_TextChanged);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExecute.Location = new System.Drawing.Point(785, 12);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(85, 24);
+            this.btnExecute.TabIndex = 5;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // Viewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 510);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.inputQuery);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Viewer";
+            this.Text = "SQLite Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox inputQuery;
+        private System.Windows.Forms.Button btnExecute;
+    }
+}
---
+++ SQLiteViewer/Viewer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace SQLiteViewer
+{
+    public partial class Viewer : Form
+    {
+        private string seperator = "|^*|";
+        private DataTable dt;
+
+        private string adbPath;
+        private string packageName;
+        private string databaseName;
+
+        public Viewer()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = dt;
+            this.init();
+        }
+
+        private void init()
+        {
+            this.adbPath = ConfigurationManager.AppSettings.Get("adbPath");
+            this.packageName = ConfigurationManager.AppSettings.Get("packageName");
+            this.databaseName = ConfigurationManager.AppSettings.Get("databaseName");
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            string query = this.inputQuery.Text.Trim();
+            this.runCommand(query);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.inputQuery.Clear();
+            this.dt.Clear();
+        }
+
+        private void runCommand(string query)
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.WorkingDirectory = this.adbPath;
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+            cmd.StandardInput.WriteLine("adb shell");
+            cmd.StandardInput.WriteLine("cd data/data/{0}/databases", this.packageName);
+            cmd.StandardInput.WriteLine("sqlite3 {0}", this.databaseName);
+            cmd.StandardInput.WriteLine(".header on");
+            cmd.StandardInput.WriteLine(".separator " + "'" + this.seperator + "'");
+            cmd.StandardInput.WriteLine(query);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+
+            //string output = cmd.StandardOutput.ReadToEnd();
+            int lineCounter = 0;
+            string line;
+            bool areColumnsAdded = false;
+            this.dt = new DataTable();
+
+            while ((line = cmd.StandardOutput.ReadLine()) != null)
+            {
+                //while (!cmd.StandardOutput.EndOfStream)
+
+                lineCounter++;
+                if (lineCounter < 5) continue;
+                //Console.WriteLine(line);
+
+                if (string.IsNullOrEmpty(line)) break;
+
+                if (areColumnsAdded)
+                {
+                    this.addDataToListView(line);
+                }
+                else
+                {
+                    this.addColumnsToListView(line);
+                    areColumnsAdded = true;
+                }
+            }
+
+            cmd.WaitForExit();
+
+        }
+
+        private void addColumnsToListView(string line)
+        {
+            string[] columns = line.Split(new string[] { seperator }, StringSplitOptions.None);
+
+            //Console.WriteLine(line);
+
+            foreach (string columnName in columns)
+            {
+                this.dt.Columns.Add(columnName);
+            }
+
+            dataGridView1.DataSource = dt;
+        }
+
+        private void addDataToListView(string line)
+        {
+            string[] data = line.Split(new string[] { seperator }, StringSplitOptions.None);
+
+            //Console.WriteLine(line);
+
+            DataRow dr = dt.NewRow();
+            dr.ItemArray = data;
+            dt.Rows.Add(dr);
+        }
+
+        private void inputQuery_TextChanged(object sender, EventArgs e)
+        {
+            string tokens = "(select|SELECT|WHERE|FROM)";
+
+            Regex rex = new Regex(tokens);
+            MatchCollection mc = rex.Matches(inputQuery.Text);
+            int StartCursorPosition = inputQuery.SelectionStart;
+            foreach (Match m in mc)
+            {
+                int startIndex = m.Index;
+                int StopIndex = m.Length;
+                inputQuery.Select(startIndex, StopIndex);
+                inputQuery.SelectionColor = Color.Blue;
+                inputQuery.SelectionStart = StartCursorPosition;
+                inputQuery.SelectionColor = Color.Black;
+            }
+        }
+    }
+}
